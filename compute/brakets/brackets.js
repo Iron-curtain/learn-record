9a57cbc6860fe9b484f85bd922ec968383eb276c
Iron-curtain <2544493807@qq.com>
2020-10-28 22:57:23 +0800

    有效的括号

--- compute/brakets/brackets.js
+++ compute/brakets/brackets.js
@@ -3,10 +3,14 @@ var isValid = function(s) {
     var top = -1
     for(var i = 0; i < s.length; i++){
         (s[i] == '{' || s[i] == '[' || s[i] == '(') && (bks[++top] = s[i]);
-        if((s[i] == '}' || s[i] == ']' || s[i] == ')') && top == -1)  return false;
-        ((bks[top] == '{' && s[i] == '}') || (bks[top] == '[' && s[i] == ']') || (bks[top] == '(' && s[i] == ')') || top < 0) && top-- ;
-        console.log(top);
+        if(s[i] == '}' || s[i] == ']' || s[i] == ')')  {
+            if((bks[top] == '{' && s[i] == '}') || (bks[top] == '[' && s[i] == ']') || (bks[top] == '(' && s[i] == ')')){
+                top--
+            }
+            else
+                return false
+        }
     }
     return top == -1 
 };
-console.log(isValid("(])"));
+console.log(isValid("}{"));
