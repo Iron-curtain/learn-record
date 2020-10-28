b8efbc63c09efed1bdb53425cad05d583b4a9cc4
Iron-curtain <2544493807@qq.com>
2020-10-28 22:59:48 +0800

    栈学习

---
+++ js/datastruct/stack/StackObj.ts
@@ -0,0 +1,62 @@
+// 存储结构 JSONObject
+// O(n) O(1)
+interface StackObj {
+    [propName: number] : any;
+}
+
+export default class ObjStack {
+    // 定义栈并规定其类型
+    private items: StackObj  // 先声明再用
+    private count: number // array .length - 1
+
+    constructor() {
+        this.items = {}
+        this.count = 0
+    }
+
+    push(item: any) {
+        this.items[this.count] = item;
+        this.count++
+    }
+
+    pop(){
+        if(this.isEmpty()){
+            return undefined
+        }
+        this.count--
+        const result = this.items[this.count];
+        delete this.items[this.count];
+        return result
+    }
+
+    peak(){
+        if(this.isEmpty()){
+            return undefined
+        }
+        return this.items[this.count - 1]
+    }
+
+    isEmpty() {
+        return this.count === 0;
+    }
+
+    clear(){
+        this.items = {}
+        this.count = 0
+    }
+
+    size() :number {
+        return this.count
+    }
+
+    toString() {
+        if(this.isEmpty()) {
+            return ""
+        }
+        let objString = `${this.items[0]}`
+        for(let i = 1; i < this.count; i++){
+            objString = `${objString}, ${this.items[i]}`
+        }
+        return objString
+    }
+}
