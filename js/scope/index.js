e32537895d547d53c541c5959f846e0e29b8e89c
Iron-curtain <2544493807@qq.com>
2020-10-30 21:35:44 +0800

    作用域学习

---
+++ js/scope/index.js
@@ -0,0 +1,13 @@
+(function (str, a) {
+    // eval(str)
+    console.log(a, b);
+    function test(){
+        console.log(a, b);
+    }
+    test()
+})()
+var b = 1
+foo('var b = 3', 1)
+
+
+console.log(a);
