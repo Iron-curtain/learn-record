f9039feb49321bebf37edcbcd649d39aafbb045f
Iron-curtain <2544493807@qq.com>
2020-11-13 22:16:23 +0800

    node-learn

---
+++ node/node-learn/commonJS/index.js
@@ -0,0 +1,6 @@
+console.log('start require');
+require('./lib.js')
+console.log('end require');
+
+
+// exports导出的引用跟index.js里面的lib是同一个引用
---
+++ node/node-learn/index.js
@@ -0,0 +1,9 @@
+// console.log(requestAnimationframe); // 浏览器运行的下一帧，因为不是浏览器环境，所以不存在
+// console.log(setImmediate);
+
+// console.log(__filename);    // 当前所运行的脚本所在的位置
+// console.log(__dirname);     // 当前所运行的脚本所在的目录位置
+
+
+// 进程对象
+console.log(process);
