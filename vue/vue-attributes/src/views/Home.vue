6ab8efbfed32a23ce8c7101d86171f1bdc42ec52
Iron-curtain <2544493807@qq.com>
2020-11-28 13:07:20 +0800

    vue-attributes

---
+++ vue/vue-attributes/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <p>num: {{num}}</p>
+    <p>
+      <button @click="add">add</button>
+    </p>
+  </div>
+</template>
+
+<script>
+import { addLog, app } from './home.js'
+// import addLog from './home.js'
+export default {
+  data () {
+    return {
+      num: 1
+    }
+  },
+  mixins: [addLog], // 混入
+  extends: app,
+  methods: {
+    add () {
+      this.num++
+      console.log(this.num ** 2)
+    }
+  }
+}
+</script>
+<style>
+
+</style>
