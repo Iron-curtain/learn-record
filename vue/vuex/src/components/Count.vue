7f0ffc37e7771ce2a8877c45c9f004234699bd5e
Iron-curtain <2544493807@qq.com>
2020-12-09 23:06:55 +0800

    vuex

---
+++ vue/vuex/src/components/Count.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="count">
+    <h3>{{$store.state.count}}</h3>
+    <h2>{{newCount}}</h2>
+    <h1>{{msg}}</h1>
+    <p>
+      <button @click="addAction">add</button>
+      <button @click="$store.commit('reduce')">reduce</button>
+    </p>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations, mapGetters, mapActions } from 'vuex'
+export default {
+  // computed: {
+  //   newCount () {
+  //     return this.$store.state.count * 2
+  //   }
+  // }
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  // computed: mapState({
+  //   newCount: state => state.count 
+  // }),
+  computed: {
+    ...mapState(['state']),
+    ...mapGetters(['newCount'])
+  },
+  // methods: mapMutations(['add', 'reduce']),
+  methods: {
+    ...mapMutations(['add', 'reduce']),
+    ...mapActions(['addAction'])
+  },
+  mounted() {
+    this.msg = mapState(['count'])
+  }
+}
+</script>
+
+<style>
+
+</style>
