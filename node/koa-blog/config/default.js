52fb7f1ce9039421eacdb14c743bffa2feb6ce76
Iron-curtain <2544493807@qq.com>
2020-12-14 21:06:42 +0800

    blog

---
+++ node/koa-blog/config/default.js
@@ -0,0 +1,13 @@
+const config = {
+  // 启动的端口
+  port: 3000,
+  database: {
+    DATABASE: 'nodesql',
+    USERNAME: 'root',
+    PASSWORD: '123456',
+    PORT: '3306',
+    HOST: 'localhost'
+  }
+}
+
+module.exports = config
