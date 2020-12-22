0b0e3593f75c9aa27588486ea9a89ded98e42dec
Iron-curtain <2544493807@qq.com>
2020-12-22 22:53:29 +0800

    验证码

---
+++ vue/verify-code/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <verify-code></verify-code>
+  </div>
+</template>
+
+<script>
+import verifyCode from './verify-code.vue'
+export default {
+  components: {
+    verifyCode
+  }
+}
+</script>
+
+<style>
+
+</style>
