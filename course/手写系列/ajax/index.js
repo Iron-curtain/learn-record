2df7c2dc3e39fb0ef7f038b7627bdffa071b19a2
Iron-curtain <2544493807@qq.com>
2021-01-05 22:39:20 +0800

    ajax

--- course/手写系列/ajax/index.js
+++ course/手写系列/ajax/index.js
@@ -1,14 +1,14 @@
-let xhr = new XMLHttpRequest()
-// 初始化
-xhr.open(method, url, async)
-// 发送请求
-xhr.send(data)
-// 设置状态变化回调处理请求结果
-xhr.onreadystatechange = () => {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    console.log(xhr.responseText);
-  }
-}
+// let xhr = new XMLHttpRequest()
+// // 初始化
+// xhr.open(method, url, async)
+// // 发送请求
+// xhr.send(data)
+// // 设置状态变化回调处理请求结果
+// xhr.onreadystatechange = () => {
+//   if (xhr.readyState === 4 && xhr.status === 200) {
+//     console.log(xhr.responseText);
+//   }
+// }
 
 // 基于promise实现ajax
 function ajax(options) {
