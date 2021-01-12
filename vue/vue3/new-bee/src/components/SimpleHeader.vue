bbc3e0ec826033d94dba1c9a1c61c9e05dea998f
Iron-curtain <2544493807@qq.com>
2021-01-12 21:00:59 +0800

    新峰商城

---
+++ vue/vue3/new-bee/src/components/SimpleHeader.vue
@@ -0,0 +1,53 @@
+<template>
+  <header class="simple-header">
+    <van-icon name="arrow-left" @click="goBack"/>
+    <div class="simple-header-name">{{name}}</div>
+    <van-icon name="weapp-nav" />
+  </header>
+</template>
+
+<script>
+import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+export default {
+  props: {
+    name: {
+      type: String,
+      default: ''
+    },
+  },
+  setup(props) {
+    // const name = ref(props.name)
+    const router = useRouter()
+    const goBack = () => {
+      router.go(-1)
+    }
+    return {
+      // name,
+      goBack
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import '../common/style/mixin.less';
+.simple-header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  .fj();
+  .wh(100%, 44px);
+  line-height: 44px;
+  padding: 0 10px;
+  .boxSizing();
+  color: #252525;
+  background: #fff;
+  border-bottom: 1px solid #dcdcdc;
+  font-size: 14px;
+  .van-icon{
+    line-height: 44px;
+  }
+}
+</style>
