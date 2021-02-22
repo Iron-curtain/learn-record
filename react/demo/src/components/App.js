2c8dc9049804ada006e7cd775c32ec7457cefe5d
Iron-curtain <2544493807@qq.com>
2021-02-22 14:07:43 +0800

    react入门

---
+++ react/demo/src/components/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class App extends Component{
+  render() {
+    return  <div>hello</div> // jsx = js + XML
+      // <div>
+      //   Hello world!
+      // </div>
+    
+  }
+}
+
+export default App
---
+++ react/demo/src/components/eat.js
@@ -0,0 +1,33 @@
+import React, {Component, Fragment} from 'react'
+
+class Eat extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputValue: 'hello',
+      list: []
+    }
+  }
+
+  inputChange(e) {
+    // console.log(e.target.value);
+    // this.state.inputValue = e.target.value
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div><input value={this.state.inputValue} onChange={this.inputChange.bind(this)} /><button>下单</button></div>
+        <ul>
+          <li>宫爆鸡丁</li>
+          <li>醋溜肥肠</li>
+        </ul>
+      </Fragment>
+    )
+  }
+}
+
+export default Eat
