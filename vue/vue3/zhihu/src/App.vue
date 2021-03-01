349ae8a3f7203d93d0d647051a9e62edf97a1c33
Iron-curtain <2544493807@qq.com>
2021-03-01 23:06:52 +0800

    导航栏

---
+++ vue/vue3/zhihu/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <global-header :user="currentUser"></global-header>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import GlobalHeader from './components/GlobalHeader.vue'
+
+export default defineComponent({
+  name: 'App',
+  setup () {
+    const currentUser = {
+      isLogin: true,
+      nickName: '露西',
+      // _id: number,
+      // column: number,
+      // email: number
+    }
+    return {
+      currentUser
+    }
+  },
+  components: {
+    GlobalHeader
+  }
+})
+</script>
+
+<style>
+
+</style>
