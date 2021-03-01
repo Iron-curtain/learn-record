4a1f1c8b7990880d4461bc9617d7d9453fa29a83
Iron-curtain <2544493807@qq.com>
2021-03-01 23:04:19 +0800

    修改个人信息

--- vue/vue3/pecker/pecker-server/routes/userInfo.js
+++ vue/vue3/pecker/pecker-server/routes/userInfo.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 
 router.prefix('/userInfo')
 
+// 修改头像
 router.post('/changeAvatar', async function (ctx, next) {
   let req = ctx.request.body;
   const username = req.username;
@@ -26,7 +27,7 @@ router.post('/changeAvatar', async function (ctx, next) {
   let writeResult = await writePromise
   if (writeResult) {
     console.log(username);
-    await userService.updateUserInfo('avatar', imgPath)
+    await userService.updateUserInfo('avatar', imgPath, username)
     ctx.body = {
       code: 200,
       message: '上传成功'
@@ -39,6 +40,7 @@ router.post('/changeAvatar', async function (ctx, next) {
   }
 })
 
+// 修改主页背景
 router.post('/changeBackground', async function (ctx, next) {
   let req = ctx.request.body;
   const username = req.username;
@@ -72,6 +74,46 @@ router.post('/changeBackground', async function (ctx, next) {
       message: '上传失败'
     }
   }
+})
+
+// 修改信息
+router.post('/updateInfo', async function (ctx, next) {
+  let req = ctx.request.body
+  
+  let username = req.username
+
+  let nickName = req.nickName
+  let sex = req.sex
+  let birthday = req.birthday
+  let city = req.city
+  let sign = req.sign
+  let key = '', value = ''
+
+  if (nickName !== undefined) {
+    key = 'nickname',
+    value = nickName
+  } else if (sex !== undefined) {
+    key = 'sex',
+    value = sex
+  } else if (birthday !== undefined) {
+    key = 'birthday',
+    value = birthday
+  } else if (city !== undefined) {
+    key = 'city',
+    value = city
+  } else if (sign !== undefined) {
+    key = 'sign',
+    value = sign
+  }
+  
+  console.log(key, value);
+
+  await userService.updateUserInfo(key, value, username).then(() => {
+    ctx.body = {
+      code: 200,
+      message: '修改成功'
+    }
+  }, (err) => {console.log(err);})
 })
 
 module.exports = router
